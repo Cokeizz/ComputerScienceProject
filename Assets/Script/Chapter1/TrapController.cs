dcf2c52f5be05ee4dd65f544edee175a1d1db644
Akkaradet Wittayawuttirat <akrd.akrd@kkumail.com>
2023-08-01 15:34:13 +0700

    Add SpikeTrap

--- Assets/Script/Chapter1/TrapController.cs
+++ Assets/Script/Chapter1/TrapController.cs
@@ -2,69 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombTrapController : MonoBehaviour
+public class TrapController : MonoBehaviour
 {
-   
+
     [SerializeField] private float attackCooldown;
     [SerializeField] private int damage;
     [SerializeField] private float range;
     [SerializeField] private float colliderDistance;
-    [SerializeField]private BoxCollider2D boxCollider;
-    [SerializeField]private LayerMask playerLayer;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
 
     //References
     private Animator anim;
     private Health playerHealth;
-    private EnemyPatrol enemyPatrol;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     // Update is called once per frame
     void Update()
     {
         cooldownTimer += Time.deltaTime;
-
         //Attack when player in sight?
-        if(PlayerInSight())
+        if (PlayerInSight())
         {
-            if(cooldownTimer >= attackCooldown)
+            if (cooldownTimer >= attackCooldown)
             {
                 //Attack
                 cooldownTimer = 0;
-                anim.SetTrigger("explode");
-            }   
+                anim.SetTrigger("damage");
+            }
         }
-        if(enemyPatrol != null)
-            enemyPatrol.enabled = !PlayerInSight();
-        
     }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center+transform.right*range*transform.localScale.x*colliderDistance,
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-            0, Vector2.left,0,playerLayer);
+            0, Vector2.left, 0, playerLayer);
 
-        if(hit.collider != null)
+        if (hit.collider != null)
             playerHealth = hit.transform.GetComponent<Health>();
 
-         
+
         return hit.collider != null;
     }
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center+transform.right*range*transform.localScale.x*colliderDistance, 
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 
     private void DamagePlayer()
     {
-        if(PlayerInSight())
+        if (PlayerInSight())
         {
             //Damage Player health
             playerHealth.TakeDamage(damage);
