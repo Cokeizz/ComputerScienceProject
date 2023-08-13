da978a3d9e59587a100e674737021f6b3a63d080
RattanapornS <rattanaporn.su@kkumail.com>
2023-08-13 19:40:45 +0700

    Add Chapter3 First Commit

--- Assets/Script/Chapter1/CameraController.cs
+++ Assets/Script/Chapter1/CameraController.cs
@@ -23,22 +23,22 @@ public class CameraController : MonoBehaviour
         //Follow Player
          lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
          lookDown = Mathf.Lerp(lookDown, (aboveDistance * player.localScale.y), Time.deltaTime * cameraSpeed);
-        transform.position = new Vector3(player.position.x + lookAhead,cameraVertical+lookDown, transform.position.z);
+        transform.position = new Vector3(player.position.x + lookAhead,player.position.y+cameraVertical+lookDown, transform.position.z);
 
-        if(player.position.x<=100.0f)
-            aboveDistance = 0.0f;
+        // if(player.position.x<=100.0f)
+        //     aboveDistance = 0.0f;
 
-        if(player.position.x>=100.0f && player.position.y>=-4.3f)
-            aboveDistance = -1.0f;
+        // if(player.position.x>=100.0f && player.position.y>=-4.3f)
+        //     aboveDistance = -1.0f;
 
-        if(player.position.x>=100.0f && player.position.y<=-4.3f )
-            aboveDistance = -4.5f;
+        // if(player.position.x>=100.0f && player.position.y<=-4.3f )
+        //     aboveDistance = -4.5f;
 
-        if(player.position.x>=221.0f && player.position.y>=-4.3f)
-            aboveDistance = 0.0f;
+        // if(player.position.x>=221.0f && player.position.y>=-4.3f)
+        //     aboveDistance = 0.0f;
         
-        if(player.position.x>=375.0f && player.position.x<=402.0f)
-            aboveDistance = -1.0f;
+        // if(player.position.x>=375.0f && player.position.x<=402.0f)
+        //     aboveDistance = -1.0f;
 
         
             
