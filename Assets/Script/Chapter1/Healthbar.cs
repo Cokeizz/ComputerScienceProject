6a78d6ee31a3d2b187ec7e26175d5060a2b1d3a6
Cokeizz <akrd.akrd@kkumail.com>
2023-10-16 00:44:55 +0700

    Add Dead Event

--- Assets/Script/Chapter1/Healthbar.cs
+++ Assets/Script/Chapter1/Healthbar.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Health playerHealth;
     [SerializeField] private Image totalhealthBar;
     [SerializeField] private Image currenthealthBar;
+    [SerializeField] private Transform player;
 
     private void Start()
     {
@@ -14,5 +16,16 @@ public class Healthbar : MonoBehaviour
     private void Update()
     {
         currenthealthBar.fillAmount = playerHealth.currentHealth/6 ;
+        
+        if(currenthealthBar.fillAmount <= 0)
+        {
+            Debug.Log("Player Die");
+            SceneManager.LoadScene(5);
+        }
+
+        if(player.position.y <= -20.0f)
+        {
+            SceneManager.LoadScene(5);
+        }
     }
 }
